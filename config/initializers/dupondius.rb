1fe18e01b14a694f52ea8ab72554624333cbabe6
Stephen Bartlett <stephenb@rtlett.org>
2012-12-18 16:18:55 +1100

    Read cloudformation bucket from environment

--- config/initializers/dupondius.rb
+++ config/initializers/dupondius.rb
@@ -2,7 +2,7 @@ require 'dupondius'
 
 Dupondius.configure do |config|
   config.key_name = ENV['KEY_NAME'] || 'zerobot'
-  config.cloudformation_bucket = 'dupondius_cf_templates'
+  config.cloudformation_bucket = ENV['CLOUDFORMATION_BUCKET'] || 'dupondius_cf_templates'
   config.config_bucket = 'dupondius_config'
   config.aws_region = ENV['AWS_REGION']
   config.access_key = ENV['AWS_ACCESS_KEY_ID']
