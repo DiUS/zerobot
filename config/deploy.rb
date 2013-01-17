6b746bb0be48bd912eee919d8604700fcb4b9df7
azzamallow <azzamallow@gmail.com>
2013-01-18 10:00:01 +1100

    deploy will now pass through the auth enabled env var

--- config/deploy.rb
+++ config/deploy.rb
@@ -100,6 +100,7 @@ namespace :foreman do
       "echo \"LAUNCHPAD_JOBS=#{ENV['LAUNCHPAD_JOBS']}\" >> ./tmp/env",
       "echo \"AWS_REGION=#{ENV['AWS_REGION']}\" >> ./tmp/env",
       "echo \"DEMO_ENABLED=#{ENV['DEMO_ENABLED']}\" >> ./tmp/env",
+      "echo \"AUTH_ENABLED=#{ENV['AUTH_ENABLED']}\" >> ./tmp/env",
 
       # Push the database environment variables into the app
       "cat /etc/default/app >> ./tmp/env",
