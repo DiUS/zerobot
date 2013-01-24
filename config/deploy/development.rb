dc2f32e712dd28132f884d4f4bc39fa03f8b15f3
david <david@ubuntu.(none)>
2013-01-24 14:51:33 +1100

    fixing hard coded zerobot.io references and replacing them with the environment variable reference

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
-set :domain,      "dev.#{ENV['PROJECT_NAME']}.zerobot.io"
+set :domain,      "dev.#{ENV['PROJECT_NAME']}.#{ENV['ZONE']}"
 set :rails_env,   "development"
 set :app_env,     "development"
 set :branch,      ENV["PIPELINE_VERSION"] || 'master'
