8302109500e01493788e8906afc0d25a1f706e46
Stephen Bartlett <stephenb@rtlett.org>
2013-02-06 08:17:50 +1100

    Minor refactor to method names

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -9,34 +9,32 @@ class GithubUser
   end
 
   def login
-    get_user['login']
+    user['login']
   end
 
   def avatar_url
-    get_user['avatar_url']
+    user['avatar_url']
   end
 
-  def repos
-    (get_user_repos + get_org_repos).collect { |r| { :name => r['name'], :html_url => r['html_url'] }}.sort_by{ |r| r[:name].downcase }
+  def user_repos
+    Yajl.load(RestClient.get("https://api.github.com/user/repos", :params => {:access_token => @token, :type => 'owner'}))
   end
 
-  def get_org_repos
-    get_user_orgs.collect do |org|
+  def org_repos
+    orgs.collect do |org|
       Yajl.load(RestClient.get("https://api.github.com/orgs/#{org['login']}/repos", :params => {:access_token => @token}))
     end.flatten
   end
 
-  def get_user_repos
-    @repo_info ||= Yajl.load(RestClient.get("https://api.github.com/user/repos", :params => {:access_token => @token, :type => 'owner'}))
+  def orgs
+    @orgs ||= Yajl.load(RestClient.get("https://api.github.com/user/orgs", :params => {:access_token => @token}))
   end
-  
+
+
   private
 
-  def get_user
-    @user_info ||= Yajl.load(RestClient.get("https://api.github.com/user", :params => {:access_token => @token}))
+  def user
+    @user ||= Yajl.load(RestClient.get("https://api.github.com/user", :params => {:access_token => @token}))
   end
 
-  def get_user_orgs
-    @user_orgs ||= Yajl.load(RestClient.get("https://api.github.com/user/orgs", :params => {:access_token => @token}))
-  end
 end
