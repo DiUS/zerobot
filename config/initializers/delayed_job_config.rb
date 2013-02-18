a13f75e927072ba31cad058608f5c9a54abdebdb
Stephen Bartlett <stephenb@rtlett.org>
2013-02-18 17:09:18 +1100

    Removed delayed job. Nothing is run in the background anymore

--- config/initializers/delayed_job_config.rb
+++
@@ -1,6 +0,0 @@
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 3.minutes
-Delayed::Worker.read_ahead = 10
-Delayed::Worker.delay_jobs = !Rails.env.test?
